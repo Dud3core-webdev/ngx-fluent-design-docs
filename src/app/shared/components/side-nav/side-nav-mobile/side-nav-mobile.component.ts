eef8c9b59734efe46de5fb1075e23f40fb205757
Dud3core-webdev <dudecor3webdev@gmail.com>
2022-02-02 19:45:30 +0000

    That was surprisingly easy lol

--- src/app/shared/components/side-nav/side-nav-mobile/side-nav-mobile.component.ts
+++ src/app/shared/components/side-nav/side-nav-mobile/side-nav-mobile.component.ts
@@ -1,22 +1,37 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ApplicationNavigationLinks } from '../app-nav-links.interface';
+import { Event, NavigationEnd, Router } from '@angular/router';
 
 @Component({
     selector: 'side-nav-mobile',
     templateUrl: './side-nav-mobile.component.html',
     styleUrls: ['./side-nav-mobile.component.scss']
 })
-export class SideNavMobileComponent {
+export class SideNavMobileComponent implements OnInit {
     @Input() public readonly sideNavLinks: ApplicationNavigationLinks = [];
     @Input() public readonly isProdEnvironment: boolean = false;
 
+    private readonly _router: Router;
     private _isNavMenuOpen: boolean = false;
 
     public get isNavMenuOpen(): boolean {
         return this._isNavMenuOpen;
     }
 
-    public setIsNavMenuOpen(isNavMenuOpen: boolean) {
+    constructor(router: Router) {
+        this._router = router;
+    }
+
+    public ngOnInit(): void {
+        this._router.events
+            .subscribe((event: Event) => {
+                if (event instanceof NavigationEnd) {
+                    this.setIsNavMenuOpen(false);
+                }
+            });
+    }
+
+    public setIsNavMenuOpen(isNavMenuOpen: boolean): void {
         this._isNavMenuOpen = isNavMenuOpen;
     }
 }
