194e042dfba243f230fbf3ac0532ac7dff793239
Dud3core-webdev <dudecor3webdev@gmail.com>
2022-02-05 11:41:38 +0000

    Ammended welcome page to have better setup documentation

---
+++ src/assets/docs/code-examples/setup/browser-animations-module-import.example.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+    imports: [
+        BrowserAnimationsModule /** Make sure you have this Module! */
+    ],
+    declarations: [],
+    exports: []
+})
+export class AppModule { }
