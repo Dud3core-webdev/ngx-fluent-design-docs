cb8f6e47ca9987cf2b4c9fcfe30a967457845b3d
Dud3core-webdev <dudecor3webdev@gmail.com>
2022-02-16 17:18:18 +0000

    Moved standard CTA docs over to new table
    Started deleting Assets, we should only really include usage examples from now on.

--- src/app/pages/calls-to-action-page/pages/standard-cta/standard-cta-page.component.ts
+++ src/app/pages/calls-to-action-page/pages/standard-cta/standard-cta-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ComponentApiDataList } from '../../../../shared/components/component-api-table/trypes/component-api-data.class';
 
 @Component({
     templateUrl: './standard-cta-page.component.html',
@@ -6,6 +7,27 @@ import { Component } from '@angular/core';
 })
 export class StandardCtaPageComponent {
     public standardButtonsDisabled: boolean = false;
+    public apiData: ComponentApiDataList = [
+        {
+            name: 'disabled: boolean',
+            isRequired: false,
+            description: 'Sets a disabled class on the component. If not passed in, it will default to false.',
+            isInput: true
+        },
+        {
+            name: 'type: NgxFluentDesignCtaType',
+            isRequired: false,
+            description: 'Sets the type of the CTA, you can use either standard or primary. If not passed in, it will default to standard.',
+            isInput: true
+        },
+        {
+            name: 'clicked: EventEmitter<void>',
+            isRequired: false,
+            isOutput: true,
+            description: 'Emits every time the CTA is clicked.'
+        }
+    ];
+
 
     public toggleStandardButtons(): void {
         this.standardButtonsDisabled = !this.standardButtonsDisabled;
