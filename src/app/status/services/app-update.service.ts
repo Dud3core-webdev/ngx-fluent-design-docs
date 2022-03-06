fe7cc6e054fe4c94ea7519b59ef94bbd0f402718
Dud3core-webdev <dudecor3webdev@gmail.com>
2022-03-06 10:35:38 +0000

    Check for status updates every 2 mins

--- src/app/status/services/app-update.service.ts
+++ src/app/status/services/app-update.service.ts
@@ -33,7 +33,7 @@ export class AppUpdateService {
     }
 
     private initialise(): void {
-        const checkForUpdatesInterval: Observable<number> = interval(10000);
+        const checkForUpdatesInterval: Observable<number> = interval(120000);
         if (this._updates.isEnabled) {
             checkForUpdatesInterval
                 .pipe(
